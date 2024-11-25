0de53259aaec43cc79c3ff83832f134fee4a170b
MrBrooks89 <ericbrooks269@gmail.com>
2024-11-24 21:10:58 -0600

    added low latency pipwire config

--- .dotfiles/configuration.nix
+++ .dotfiles/configuration.nix
@@ -84,6 +84,26 @@
     pulse.enable = true;
   };
 
+   services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+    "context.properties" = [
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = { };
+      }
+    ];
+    "pulse.properties" = {
+      "pulse.min.req" = "32/48000";
+      "pulse.default.req" = "32/48000";
+      "pulse.max.req" = "32/48000";
+      "pulse.min.quantum" = "32/48000";
+      "pulse.max.quantum" = "32/48000";
+    };
+    "stream.properties" = {
+      "node.latency" = "32/48000";
+      "resample.quality" = 1;
+    };
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
     defaultUserShell = pkgs.zsh;
