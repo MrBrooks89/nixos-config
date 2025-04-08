f69dc7e043c263b7d47ba83971f96d99fe098449
MrBrooks89 <ericbrooks269@gmail.com>
2025-04-08 18:45:31 -0500

    added .dotfiles back to home directory

---
+++ .dotfiles/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "My flake for multiple users";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin.url = "github:catppuccin/nix";
+  };
+
+  outputs = { nixpkgs, home-manager, catppuccin, ... } @ inputs:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          catppuccin.nixosModules.catppuccin
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      # Home Manager configuration for mrbrooks
+      mrbrooks = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home/mrbrooks/home.nix
+          catppuccin.homeModules.catppuccin
+        ];
+      };
+     
+     # Home Manager configuration for mrsbrooks
+     mrsbrooks = home-manager.lib.homeManagerConfiguration {
+       inherit pkgs;
+       modules = [
+         ./home/mrsbrooks/home.nix
+         catppuccin.homeModules.catppuccin
+       ];
+      };
+
+    };
+  };
+}
