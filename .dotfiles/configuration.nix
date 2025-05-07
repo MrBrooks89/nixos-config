701cdfbdebd3f4f8953a7452b1822e07235d2b82
MrBrooks89 <ericbrooks269@gmail.com>
2025-05-07 15:03:57 -0500

    updated printer IP

--- .dotfiles/configuration.nix
+++ .dotfiles/configuration.nix
@@ -117,7 +117,7 @@
      {
       name = "Canon_MB2720";
       location = "Home";
-      deviceUri = "http://192.168.5.115:631/";
+      deviceUri = "http://192.168.5.114:631/";
       model = "canonmb2700.ppd";
       ppdOptions = {
        PageSize = "Letter Small"; 
