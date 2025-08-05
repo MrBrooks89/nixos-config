cd48f1e8bee6ee79af7c8bbfb6d5c649467171a8
MrBrooks <ericbrooks269@gmail.com>
2025-08-05 09:06:22 -0500

    added sherlock for hyprland menu to replace rofi

--- .dotfiles/flake.nix
+++ .dotfiles/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
     catppuccin.url = "github:catppuccin/nix";
+    sherlock.url = "github:Skxxtz/sherlock";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -34,6 +35,7 @@
         inherit pkgs;
         modules = [
           ./home.nix
+          inputs.sherlock.homeManagerModules.default
           #catppuccin.homeModules.catppuccin
         ];
       };
--- .dotfiles/home.nix
+++ .dotfiles/home.nix
@@ -6,6 +6,7 @@ imports =
     [ # Include the results of the hardware scan.
       ./starship.nix
       ./zsh.nix
+      ./sherlock.nix
     ];
 
 
