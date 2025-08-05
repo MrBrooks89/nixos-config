7eab5b4ad0164ce85ba61f174e2dd1e3222362ab
MrBrooks <ericbrooks269@gmail.com>
2025-08-05 09:33:27 -0500

    modified sherlock settings

--- .dotfiles/sherlock.nix
+++ .dotfiles/sherlock.nix
@@ -15,6 +15,17 @@
         debug = {
           try_suppress_warnings = true;
         };
+        default_apps = {
+          teams = "teams-for-linux --enable-features=UseOzonePlatform --ozone-platform=wayland --url {meeting_url}";
+          terminal = "kitty";
+        };
+        units = {
+            lenghts = "feet";
+            weights = "lb";
+            volumens = "gal";
+            temperatures = "F";
+            currency = "usd";
+        };       
       };
 
       ignore = ''
@@ -23,11 +34,24 @@
 
       launchers = [
         {
-          name = "App Launcher";
-          type = "app_launcher";
-          args = {};
-          priority = 1;
-          home = true;
+          name = "Weather";
+          type = "Weather";
+          args = {
+            location = "Longview, TX";
+            update_interval = 60;
+          };
+        }
+        {
+            name = "Calculator";
+            type = "calculation";
+            args = {
+                capabilities = [
+                    "calc.math"
+                    "calc.units"
+                ];
+            priority = 1;
+            on_return = "copy";
+            };
         }
       ];
 
