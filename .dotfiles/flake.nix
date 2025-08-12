dab2b98b9a8885c8f2f5b3ce33456d6523e09018
MrBrooks <ericbrooks269@gmail.com>
2025-08-12 18:58:34 -0500

    changing sherlock

--- .dotfiles/flake.nix
+++ .dotfiles/flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, sherlock, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
@@ -31,12 +31,11 @@
       # Home Manager configuration for mrbrooks
       mrbrooks = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
         modules = [
           ./home.nix
+          inputs.sherlock.homeManagerModules.default
         ];
       };
-
     };
   };
 }
