d1e2f3c18bdc111d93ceed83e8296f3ceafc41dd
MrBrooks <ericbrooks269@gmail.com>
2025-08-18 11:51:40 -0500

    added optimization

--- .dotfiles/configuration.nix
+++ .dotfiles/configuration.nix
@@ -9,6 +9,7 @@
         # Bootloader.
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
+        boot.loader.systemd-boot.configurationLimit = 10;
         boot.kernelPackages = pkgs.linuxPackages_latest;
 
         networking.hostName = "nixos"; 
@@ -41,11 +42,21 @@
         boot.kernel.sysctl."net.ipv4.tcp_rmem" = "4096 87380 1073741824"; # 1 GiB max
         boot.kernel.sysctl."net.ipv4.tcp_wmem" = "4096 87380 1073741824"; # 1 GiB max
 
+        # Enable automatic garbage collection
         nix.gc = {
                 automatic = true;
                 dates = "Mon 15:30";
                 options = "--delete-older-than 20d";
         };
+
+        # Enable automatic storage optimization
+        nix.optimise = {
+                automatic = true;
+                dates = "Mon 15:30";
+        };
+
+        # Enable automatic deduplication of the Nix store
+        nix.settings.auto-optimise-store = true;
 
 
 
