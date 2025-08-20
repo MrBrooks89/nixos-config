9bbce277e585bad43ccf5ef84d0b814ba5abdf07
MrBrooks89 <ericbrooks269@gmail.com>
2025-08-20 08:55:40 -0500

    test

--- .dotfiles/configuration.nix
+++ .dotfiles/configuration.nix
@@ -6,7 +6,7 @@
                         ./hardware-configuration.nix
                         ./vm.nix
                 ];
-        # Bootloader.
+        # Bootloader. Test
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
         boot.loader.systemd-boot.configurationLimit = 10;
